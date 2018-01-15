2ca1edbae7aa2434cc8f54216ce5157a438ab79b
Akram BLOUZA <akram@MacBook-Pro-de-Akram.local>
2018-01-15 15:45:44 +0100

    remplacement des hostname

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
+
 ssh-keygen -b 2048 -t rsa -f /root/.ssh/id_rsa -q -N ""
-for host in paas.mousquetaires.com paas-node-infra.mousquetaires.com paas-node-app.mousquetaires.com; \
+for host in paas.wescale.fr paas-node-infra.wescale.fr paas-node-app.wescale.fr; \
     do ssh-copy-id -i /root/.ssh/id_rsa.pub $host; \
     done
+
 ansible-playbook /home/vagrant/openshift-ansible/playbooks/byo/config.yml
 
 
